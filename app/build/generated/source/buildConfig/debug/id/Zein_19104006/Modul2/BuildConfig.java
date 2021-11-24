22fbfbbe602e306f1451a5cc9fc669f119a7547d
Rize Hersch <34876769+IzaN32@users.noreply.github.com>
2021-11-25 01:16:58 +0700

    1st

---
+++ app/build/generated/source/buildConfig/debug/id/Zein_19104006/Modul2/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.Zein_19104006.Modul2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "id.naseh_19104003.myfirstapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
